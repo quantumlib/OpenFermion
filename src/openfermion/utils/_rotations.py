d9c72ca25040d7800875f9e9e03c8feace126e23
Kevin J. Sung <kevinjefferysung@gmail.com>
2017-10-11 09:53:46 -0400

    wrote givens_decomposition

--- src/openfermion/utils/_rotations.py
+++ src/openfermion/utils/_rotations.py
@@ -78,23 +78,29 @@ def trivial_givens_decomposition(columns):
 def givens_decomposition(unitary_rows):
     """Decompose a matrix into a sequence of Givens rotations.
 
-    The input is an m x n matrix U with m <= n. The rows of U are orthonormal.
-    U can be decomposed as follows:
+    The input is an m x n matrix Q with m <= n. The rows of Q are orthonormal.
+    Q can be decomposed as follows:
 
-        U * G_1 * ... * G_k = D
+        V * Q * U^\dagger = D
 
-    where G_1, ..., G_k are complex Givens rotations, which are invertible n x n matrices,
-    and D is an m x n matrix with the first m columns forming a diagonal matrix and
-    the rest of the columns being zero. This gives an RQ decomposition of U.
+    where V and U are unitary matrices, and D is an m x n matrix with the
+    first m columns forming a diagonal matrix and the rest of the columns being zero.
+    Furthermore, we can decompose U as
+
+        U = G_k * ... * G_1
+
+    where G_1, ..., G_k are complex Givens rotations, which are invertible n x n matrices.
     We describe a complex Givens rotation by the column indices (i, j) that it
     acts on, plus two angles (theta, phi) that characterize the corresponding
     2x2 unitary matrix
+
         [ cos(theta)    -e^{i phi} sin(theta) ]
         [ sin(theta)     e^{i phi} cos(theta) ]
 
     Args:
-        rows: A numpy array or matrix with orthonormal rows.
+        unitary_rows: A numpy array or matrix with orthonormal rows.
     Returns:
+        V: An m x m numpy array.
         givens_rotations: A list of tuples of objects describing Givens rotations.
             The list looks something like [(G1, ), (G2, G3), ... ].
             The Givens rotations within a tuple can be implemented in parallel.
@@ -106,54 +112,112 @@ def givens_decomposition(unitary_rows):
     rows = numpy.copy(unitary_rows)
     m, n = rows.shape
 
-    givens_rotations = []
+    # Compute V using Givens rotations
+    V = numpy.eye(m, dtype=complex)
+    for k in reversed(range(n - m + 1, n)):
+        # Zero out entries in column k
+        for l in range(m - n + k):
+            # Zero out entry in row l
+            G = givens_matrix_elements(rows[l, k], rows[l + 1, k])
+            expanded_G = numpy.eye(m, dtype=complex)
+            expanded_G[([l], [l + 1]), (l, l + 1)] = G
 
-    if m < n:
-        num_rows_to_zero_out = m
-    else:
-        num_rows_to_zero_out = m - 1
+            rows = expanded_G.dot(rows)
+            V = expanded_G.dot(V)
 
-    return
+    # Compute the decomposition of U into Givens rotations
+    givens_rotations = list()
+    if m != n:
+        # The circuit depth is n - 1
+        for k in range(n - 1):
+            # Get the (row, column) indices of elements to zero out in parallel
+            # Get the maximum number of simultaneous rotations
+            msr = min(m, n - m)
+            if k < msr - 1:
+                # There are k + 1 elements to zero out
+                indices_to_zero_out = zip(range(k + 1),
+                                          range(n - m - k, n - m + k + 1, 2))
+            elif k > n - msr - 1:
+                # There are n - 1 - k elements to zero out
+                indices_to_zero_out = zip(range(m - n + k + 1, m),
+                                          range(m - n + k + 2, m + n - k - 1 , 2))
+            else:
+                # There are msr elements to zero out
+                if msr == m:
+                    indices_to_zero_out = zip(range(m),
+                                              range(n - m - k, n + m - k - 1, 2))
+                else:
+                    indices_to_zero_out = zip(range(k + 1 - msr, k + 1),
+                                              range(k + 2 - msr, k + 1 + msr, 2))
+
+            parallel_rotations = list()
+            for i, j in indices_to_zero_out:
+                print(i, j)
+                # Compute the Givens rotation to zero out the (i, j) element
+                a = rows[i, j - 1].conj()
+                b = rows[i, j].conj()
+                G = givens_matrix_elements(a, b)
+                G = G[(1, 0), :]
+
+                # Add the parameters to the list
+                theta = numpy.arccos(numpy.real(G[0, 0]))
+                phi = numpy.angle(G[1, 1])
+                parallel_rotations.append((i, j, theta, phi))
+
+                # Update the matrix
+                expanded_G = numpy.eye(n, dtype=complex)
+                expanded_G[([j - 1], [j]), (j - 1, j)] = G
+                rows = rows.dot(expanded_G.T.conj())
+            print(rows)
+            print()
+
+            # Append the current list of parallel rotations to the list
+            givens_rotations.append(parallel_rotations)
+
+    print(rows)
+    diagonal = rows.diagonal()
+
+    return V, givens_rotations, diagonal
 
 def givens_matrix_elements(a, b):
     """Compute the matrix elements of the Givens rotation that zeroes out one of two
-    column entries.
+    row entries.
 
     Returns a matrix G such that
 
-        [a  b] * G = [r  0]
+        G * [a  b]^T= [0  r]^T
 
     where r is a complex number.
 
     Args:
-        a: A complex number representing the left-hand column entry
-        b: A complex number representing the right-hand column entry
+        a: A complex number representing the upper row entry
+        b: A complex number representing the lower row entry
     Returns:
         G: A 2 x 2 numpy array representing the matrix G. The numbers in the
-            first row of G are real.
+            first column of G are real.
     """
-    # Handle case that b is zero
-    if abs(b) < EQ_TOLERANCE:
+    # Handle case that a is zero
+    if abs(a) < EQ_TOLERANCE:
         c = 1.
         s = 0.
         phase = 1.
-    # Handle case that a is zero and b is nonzero
-    elif abs(a) < EQ_TOLERANCE:
+    # Handle case that b is zero and a is nonzero
+    elif abs(b) < EQ_TOLERANCE:
         c = 0.
         s = 1.
-        sign_b = b / abs(b)
-        phase = -sign_b.conjugate()
+        sign_a = a / abs(a)
+        phase = sign_a
     # Handle case that a and b are both nonzero
     else:
         denominator = numpy.sqrt(abs(a) ** 2 + abs(b) ** 2)
 
-        c = abs(a) / denominator
-        s = abs(b) / denominator
-        sign_a = a / abs(a)
+        c = abs(b) / denominator
+        s = abs(a) / denominator
         sign_b = b / abs(b)
-        phase = -sign_a * sign_b.conjugate()
+        sign_a = a / abs(a)
+        phase = sign_a * sign_b.conjugate()
 
     # Construct matrix and return
-    G = numpy.array([[c, s],
-                     [-phase * s, phase * c]], dtype=complex)
+    G = numpy.array([[c, -phase * s],
+                     [s, phase * c]], dtype=complex)
     return G
