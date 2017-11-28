ea8d0648cb706deef3fbf8a3e4f7b7645bde7ef2
Chengyu Dai <jdaaph@gmail.com>
2017-11-28 08:48:25 -0500

    eliminate the stray byte xe2

--- src/openfermion/utils/_bch_expansion_test.py
+++ src/openfermion/utils/_bch_expansion_test.py
@@ -10,7 +10,6 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from __future__ import absolute_import
 import unittest
 from numpy.random import rand, seed
 from numpy.linalg import norm
