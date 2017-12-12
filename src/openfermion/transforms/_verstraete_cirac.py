2febe2b0ba70e2667a586975c5b70fa69a51704f
Kevin J. Sung <kevinjefferysung@gmail.com>
2017-12-12 18:09:12 -0500

    update description

--- src/openfermion/transforms/_verstraete_cirac.py
+++ src/openfermion/transforms/_verstraete_cirac.py
@@ -10,7 +10,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-"""Jordan-Wigner transform on fermionic operators."""
+"""Verstraete-Cirac transform on fermionic operators."""
 from __future__ import absolute_import
 
 import itertools
