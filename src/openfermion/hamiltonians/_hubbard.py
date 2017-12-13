bce3dbe724bbe5b21679648b9a99bb9fa19187ad
Kevin J. Sung <kevinjefferysung@gmail.com>
2017-12-13 16:51:15 -0500

    improved documentation

--- src/openfermion/hamiltonians/_hubbard.py
+++ src/openfermion/hamiltonians/_hubbard.py
@@ -35,35 +35,47 @@ def fermi_hubbard(x_dimension, y_dimension, tunneling, coulomb,
     """Return symbolic representation of a Fermi-Hubbard Hamiltonian.
 
     The idea of this model is that some fermions move around on a grid and the
-    energy of the model depends on where the fermions are. In the standard
-    Fermi-Hubbard model (which we call the "spinful" model), there is room
-    for an "up" fermion and a "down" fermion at each site on the grid.
-    Accordingly, the Hamiltonian is
+    energy of the model depends on where the fermions are.
+    The Hamiltonians of this model live on a grid of dimensions
+    `x_dimension` x `y_dimension`.
+    The grid can have periodic boundary conditions or not.
+    In the standard Fermi-Hubbard model (which we call the "spinful" model),
+    there is room for an "up" fermion and a "down" fermion at each site on the
+    grid. In this model, there are a total of `2N` spin-orbitals,
+    where `N = x_dimension * y_dimension` is the number of sites.
+    In the spinless model, there is only one spin-orbital per site
+    for a total of `N`.
+
+    The Hamiltonian for the spinful model has the form
 
     .. math::
 
         \\begin{align}
         H = &- t \sum_{\langle i,j \\rangle} \sum_{\sigma}
                      (a^\dagger_{i, \sigma} a_{j, \sigma} +
-                     a^\dagger_{j, \sigma} a_{i, \sigma})
+                      a^\dagger_{j, \sigma} a_{i, \sigma})
              + U \sum_{i} a^\dagger_{i, \\uparrow} a_{i, \\uparrow}
                          a^\dagger_{j, \downarrow} a_{j, \downarrow}
             \\\\
             &- \mu \sum_i (a^\dagger_{i, \\uparrow} a_{i, \\uparrow} +
                          a^\dagger_{i, \downarrow} a_{i, \downarrow})
              - h \sum_i (a^\dagger_{i, \\uparrow} a_{i, \\uparrow} -
-                       a^\dagger_{i, \downarrow} a_{i, \downarrow}).
+                       a^\dagger_{i, \downarrow} a_{i, \downarrow})
         \\end{align}
 
     where
 
+        - The indices :math:`\langle i, j \\rangle` run over pairs
+          :math:`i` and :math:`j` of sites that are connected to each other
+          in the grid
         - :math:`\sigma \in \\{\\uparrow, \downarrow\\}` is the spin
         - :math:`t` is the tunneling amplitude
         - :math:`U` is the Coulomb potential
         - :math:`\mu` is the chemical potential
         - :math:`h` is the magnetic field
 
-    The code also allows one to construct the spinless Fermi-Hubbard model:
+    One can also construct the Hamiltonian for the spinless model, which
+    has the form
 
     .. math::
 
@@ -71,9 +83,6 @@ def fermi_hubbard(x_dimension, y_dimension, tunneling, coulomb,
             + U \sum_{k=1}^{N-1} a_k^\dagger a_k a_{k+1}^\dagger a_{k+1}
             + h \sum_{k=1}^N (-1)^k a_k^\dagger a_k
             - \mu \sum_{k=1}^N a_k^\dagger a_k.
-
-    These Hamiltonians live on a grid of dimensions x_dimension by y_dimension.
-    They can have periodic boundary conditions or not.
 
     Args:
         x_dimension (int): The width of the grid.
