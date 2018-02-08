3deddd68b8048cd415735b92eeb6c681232124c2
iozfidan <iozfidan@dwavesys.com>
2018-02-07 16:40:06 -0800

    prep for first pull request: The appetizer, binary_operator only

--- src/openfermion/simplifications/_code_operator_transform_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from _code_operator import BinaryCode
-from _binary_operator import SymbolicBinary
-from _code_operator_transform import code_transform
-
-class CodeTransformTest(unittest.TestCase):
-
-    def test_basic_init(self):
-        raise NotImplementedError()
