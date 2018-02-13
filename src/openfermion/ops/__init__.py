f7a8e0b3ad5277749614d7e72b8bac017e5f2485
Kevin J. Sung <kevinsung@users.noreply.github.com>
2018-02-13 06:45:56 -0800

    Create special operators module (#206)
    * create special_operators module and reorganize
    
    * edit module description

--- src/openfermion/ops/__init__.py
+++ src/openfermion/ops/__init__.py
@@ -15,9 +15,7 @@ from ._code_operator import BinaryCode
 from ._symbolic_operator import SymbolicOperator
 from ._fermion_operator import (FermionOperator,
                                 hermitian_conjugated,
-                                majorana_operator,
-                                normal_ordered,
-                                number_operator)
+                                normal_ordered)
 from ._qubit_operator import QubitOperator
 from ._polynomial_tensor import general_basis_change, PolynomialTensor
 from ._interaction_operator import InteractionOperator
