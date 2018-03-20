b5f5792e1e2d106b3343a00991a67395e097d716
Kevin J. Sung <kevinjefferysung@gmail.com>
2018-03-20 10:16:42 -0700

    define sx, sy

--- src/openfermion/utils/_special_operators_test.py
+++ src/openfermion/utils/_special_operators_test.py
@@ -16,7 +16,8 @@ import unittest
 from openfermion.ops import FermionOperator
 from openfermion.utils import (majorana_operator, s_minus_operator,
                                s_plus_operator, s_squared_operator,
-                               sz_operator, up_index, down_index)
+                               sx_operator, sy_operator, sz_operator,
+                               up_index, down_index)
 
 
 class FermionSpinOperatorsTest(unittest.TestCase):
@@ -29,27 +30,11 @@ class FermionSpinOperatorsTest(unittest.TestCase):
         self.assertTrue(numpy.isclose(down_index(2), 5))
         self.assertTrue(numpy.isclose(down_index(5), 11))
 
-    def test_sz_operator(self):
-        op = sz_operator(2)
-        expected = (FermionOperator(((0, 1), (0, 0)), 0.5) -
-                    FermionOperator(((1, 1), (1, 0)), 0.5) +
-                    FermionOperator(((2, 1), (2, 0)), 0.5) -
-                    FermionOperator(((3, 1), (3, 0)), 0.5))
-        self.assertTrue(op.isclose(expected))
-
-    def test_sz_operator_invalid_input(self):
-        with self.assertRaises(TypeError):
-            sz_operator('A')
-
     def test_s_plus_operator(self):
         op = s_plus_operator(2)
         expected = (FermionOperator(((0, 1), (1, 0))) +
                     FermionOperator(((2, 1), (3, 0))))
         self.assertTrue(op.isclose(expected))
-
-    def test_s_plus_operator_invalid_input(self):
-        with self.assertRaises(TypeError):
-            s_plus_operator('a')
 
     def test_s_minus_operator(self):
         op = s_minus_operator(3)
@@ -58,9 +43,29 @@ class FermionSpinOperatorsTest(unittest.TestCase):
                     FermionOperator(((5, 1), (4, 0))))
         self.assertTrue(op.isclose(expected))
 
-    def test_s_minus_operator_invalid_input(self):
-        with self.assertRaises(TypeError):
-            s_minus_operator('a')
+    def test_sx_operator(self):
+        op = sx_operator(2)
+        expected = (FermionOperator(((0, 1), (1, 0)), 0.5) +
+                    FermionOperator(((1, 1), (0, 0)), 0.5) +
+                    FermionOperator(((2, 1), (3, 0)), 0.5) +
+                    FermionOperator(((3, 1), (2, 0)), 0.5))
+        self.assertTrue(op.isclose(expected))
+
+    def test_sy_operator(self):
+        op = sy_operator(2)
+        expected = (FermionOperator(((0, 1), (1, 0)), -0.5j) -
+                    FermionOperator(((1, 1), (0, 0)), -0.5j) +
+                    FermionOperator(((2, 1), (3, 0)), -0.5j) -
+                    FermionOperator(((3, 1), (2, 0)), -0.5j))
+        self.assertTrue(op.isclose(expected))
+
+    def test_sz_operator(self):
+        op = sz_operator(2)
+        expected = (FermionOperator(((0, 1), (0, 0)), 0.5) -
+                    FermionOperator(((1, 1), (1, 0)), 0.5) +
+                    FermionOperator(((2, 1), (2, 0)), 0.5) -
+                    FermionOperator(((3, 1), (3, 0)), 0.5))
+        self.assertTrue(op.isclose(expected))
 
     def test_s_squared_operator(self):
         op = s_squared_operator(2)
@@ -75,7 +80,35 @@ class FermionSpinOperatorsTest(unittest.TestCase):
         expected = s_minus * s_plus + s_z * s_z + s_z
         self.assertTrue(op.isclose(expected))
 
-    def test_s_squared_operator_invalid_input(self):
+    def test_relations(self):
+        n_spatial_orbitals = 2
+
+        s_plus = s_plus_operator(n_spatial_orbitals)
+        s_minus = s_minus_operator(n_spatial_orbitals)
+        s_x = s_x_operator(n_spatial_orbitals)
+        s_y = s_y_operator(n_spatial_orbitals)
+        s_z = s_z_operator(n_spatial_orbitals)
+        s_squared = s_squared_operator(n_spatial_orbitals)
+
+        #TODO
+
+    def test_invalid_input(self):
+
+        with self.assertRaises(TypeError):
+            s_minus_operator('a')
+
+        with self.assertRaises(TypeError):
+            s_plus_operator('a')
+
+        with self.assertRaises(TypeError):
+            sx_operator('a')
+
+        with self.assertRaises(TypeError):
+            sy_operator('a')
+
+        with self.assertRaises(TypeError):
+            sz_operator('a')
+
         with self.assertRaises(TypeError):
             s_squared_operator('a')
 
