8007baa089ed8362d65193e84e329a7660be3497
Xinle Liu <lxinle@sas.upenn.edu>
2018-06-08 20:53:27 -0700

    Add Davidson algo dealing with sparse matrices.

--- src/openfermion/utils/__init__.py
+++ src/openfermion/utils/__init__.py
@@ -93,7 +93,7 @@ from ._sparse_tools import (boson_ladder_sparse,
                             sparse_eigenspectrum,
                             variance)
 
-from ._davidson import Davidson, DavidsonOptions, QubitDavidson
+from ._davidson import Davidson, DavidsonOptions, QubitDavidson, SparseDavidson
 from ._linear_qubit_operator import (
     LinearQubitOperator,
     LinearQubitOperatorOptions,
