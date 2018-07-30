0451c0857df0ad87f58f30448e4e93059feb4a13
Bryan A. O'Gorman <bryan.a.ogorman@gmail.com>
2018-07-29 21:53:29 -0700

    Made IsingOperator subclass QubitOperator

--- src/openfermion/ops/_ising_operator.py
+++ src/openfermion/ops/_ising_operator.py
@@ -16,12 +16,9 @@ Zs."""
 from collections import defaultdict
 from itertools import product, chain
 
-from openfermion.ops import SymbolicOperator
+from openfermion.ops import QubitOperator
 
-SCALARS = (int, float, complex)
-
-
-class IsingOperator(SymbolicOperator):
+class IsingOperator(QubitOperator):
     """The IsingOperator class provides an analytic representation of an
     Ising-type Hamiltonian, i.e. a sum of product of Zs.
 
@@ -40,40 +37,3 @@ class IsingOperator(SymbolicOperator):
     action_strings = ('Z')
     action_before_index = True
     different_indices_commute = True
-
-    def __imul__(self, multiplier):
-        """
-        Override in-place multiply of SymbolicOperator.
-
-        Args:
-            multiplier(int, complex, float, or IsingOperator): multiplier
-        """
-
-        # Handle scalars
-        if isinstance(multiplier, SCALARS):
-            for term in self.terms:
-                self.terms[term] *= multiplier
-            return self
-
-        # Handle non-scalar multipliers other than IsingOperators
-        if not isinstance(multiplier, self.__class__):
-            raise TypeError('Cannot in-place multiply IsingOperator by '
-                            'multiplier of type {}.'.format(type(multiplier)))
-
-        product_terms = dict()
-        term_pairs = product(self.terms.items(), multiplier.terms.items())
-        for ((left_factors, left_coefficient),
-             (right_factors, right_coefficient)) in term_pairs:
-            powers = defaultdict(int)
-            for factor in chain(left_factors, right_factors):
-                powers[factor[0]] += 1
-            product_factors = tuple((i, 'Z') for i in
-                                    sorted(i for i, p in
-                                           powers.items() if p % 2))
-            product_coefficient = left_coefficient * right_coefficient
-            if product_factors in product_terms:
-                product_terms[product_factors] += product_coefficient
-            else:
-                product_terms[product_factors] = product_coefficient
-        self.terms = product_terms
-        return self
