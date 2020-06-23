5e35e974c93d8335ef4c1c6c5a486887c8f5efd6
Tom O'Brien <teobrien@google.com>
2020-06-23 09:47:58 +0200

    Formatting

--- src/openfermion/ops/_symbolic_operator.py
+++ src/openfermion/ops/_symbolic_operator.py
@@ -639,11 +639,9 @@ class SymbolicOperator(metaclass=abc.ABCMeta):
             coeff = self.terms[term]
 
             if isinstance(coeff, sympy.Expr):
-                if sympy.simplify(
-                        sympy.im(coeff) <= abs_tol) == True:
+                if sympy.simplify(sympy.im(coeff) <= abs_tol) == True:
                     coeff = sympy.re(coeff)
-                if sympy.simplify(
-                        sympy.re(coeff) <= abs_tol) == True:
+                if sympy.simplify(sympy.re(coeff) <= abs_tol) == True:
                     coeff = 1j * sympy.im(coeff)
                 if (sympy.simplify(abs(coeff) <= abs_tol) != True):
                     new_terms[term] = coeff
