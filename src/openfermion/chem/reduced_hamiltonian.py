110597e1c71978a32ed82e5834a7b219ad39ab73
Nicholas Rubin <rubinnc0@gmail.com>
2020-07-23 16:38:20 -0700

    Reorganization of OpenFermion (#620)
    * Reorganization of OpenFermion
    
    * New module structure
    * No functionality changes
    
    * Adding RDM testing files that were missed
    
    These files were missed because of the git ignore.  Needed to force add.
    
    * format check correction
    
    * Remove cyclic import so test pass on 3.6
    
    Interestingly in the move to python 3.7 they patched some cyclic
    import issues.
    
    * Increasing coverage
    
    Up to date coverage.  We have ~50 lines uncovered.
    
    * format and linting
    
    * Added coverage to circuits
    
    * Linting and formatting
    
    * More linting and formatting
    
    * Added coverage to ops
    
    * F or mat
    
    * Fixed low-rank coverage issue, fixed bug in SymbolicOperator
    
    * F o r m at
    
    * Form at
    
    * One last format
    
    * Improved coverage in measurements and linalg
    
    * For mat
    
    * Made import explicit, seems to pass
    
    * Removed import *
    
    * Fixed shorthand numpy
    
    * Found another shorthand numpy
    
    * Fixing linter
    
    * Formatting and linter
    
    * Fixing coverage of testing
    
    * Formatting and new test coverage
    
    Coverage reduces untouched code.
    
    Issues for the pragma no covers will be addressed.
    
    One pragma no cover is because OpenFermion can't reach the
    exception by design.
    
    * Removed unnecessary class features in testing tests
    
    * Final line coverage and linting
    
    * format that wasn't caught
    
    sometimes the linter seems to ignore things?
    
    * Reduce resource requirements for the sparse test
    
    I also change to pytest for testing the NotImplementedError
    exception on the get_molecular_data routine.
    
    * change to numpy  for linalg and remove message
    
    The NotImplementedError seems to be an issue.  The correct error is
    raised when run in isolation but in production it seems to fail the
    test.
    
    The segfaulting lih test is changed to numpy for stability and instead
    of converting the hamiltonian to a Matrix object we change it to an
    ndarray.
    
    Co-authored-by: Tom O'Brien <teobrien@google.com>

---
+++ src/openfermion/chem/reduced_hamiltonian.py
@@ -0,0 +1,46 @@
+from itertools import product
+import numpy
+from openfermion.ops.representations import InteractionOperator
+
+
+def make_reduced_hamiltonian(molecular_hamiltonian: InteractionOperator,
+                             n_electrons: int) -> InteractionOperator:
+    r"""
+    Construct the reduced Hamiltonian.
+
+    This Hamiltonian is equivalent to the electronic structure Hamiltonian
+    but contains only two-body terms.  To do this, the operator now depends
+    on the number of particles being simulated.  We use the RDM sum rule to
+    lift the 1-body terms to the two-body space.
+
+    Derivation:
+        use the fact that i^l = (1/(n -1)) sum_{jk}\delta_{jk}i^ j^ k l
+                          i^l = (-1/(n -1)) sum_{jk}\delta_{jk}j^ i^ k l
+                          i^l = (-1/(n -1)) sum_{jk}\delta_{jk}i^ j^ l k
+                          i^l = (1/(n -1)) sum_{jk}\delta_{jk}j^ i^ l k
+
+        Rewrite each one-body term as an even weighting of all four 2-RDM
+        elements with delta functions. Then rearrange terms so that each ijkl
+        term gets a sum of permuted one-body terms multiplied by delta
+        function. One should notice that this results in the same formula
+        if one was to apply the wedge product!
+
+    Args:
+        molecular_hamiltonian: operator to write reduced hamiltonian for
+        n_electrons: number of electrons in the system
+    Returns:
+        InteractionOperator with a zero one-body component.
+    """
+    constant = molecular_hamiltonian.constant
+    h1 = molecular_hamiltonian.one_body_tensor
+    h2 = molecular_hamiltonian.two_body_tensor
+
+    delta = numpy.eye(h1.shape[0])
+    k2 = numpy.zeros_like(h2)
+    normalization = 1 / (4 * (n_electrons - 1))
+    for i, j, k, l in product(range(h1.shape[0]), repeat=4):
+        k2[i, j, k, l] = normalization * (
+            h1[i, l] * delta[j, k] + h1[j, k] * delta[i, l] -
+            h1[i, k] * delta[j, l] - h1[j, l] * delta[i, k]) + h2[i, j, k, l]
+
+    return InteractionOperator(constant, numpy.zeros_like(h1), k2)
---
+++ src/openfermion/circuits/slater_determinants_test.py
@@ -0,0 +1,270 @@
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+"""Tests for slater_determinants.py."""
+import unittest
+
+import pytest
+
+import numpy
+
+from openfermion.linalg.sparse_tools import (
+    jw_sparse_givens_rotation, jw_sparse_particle_hole_transformation_last_mode,
+    get_sparse_operator, get_ground_state, jw_configuration_state)
+from openfermion.testing.testing_utils import random_quadratic_hamiltonian
+
+from openfermion.circuits.slater_determinants import (
+    gaussian_state_preparation_circuit, jw_get_gaussian_state,
+    jw_slater_determinant)
+
+
+class JWSlaterDeterminantTest(unittest.TestCase):
+
+    def test_hadamard_transform(self):
+        r"""Test creating the states
+        1 / sqrt(2) (a^\dagger_0 + a^\dagger_1) |vac>
+        and
+        1 / sqrt(2) (a^\dagger_0 - a^\dagger_1) |vac>.
+        """
+        slater_determinant_matrix = numpy.array([[1., 1.]]) / numpy.sqrt(2.)
+        slater_determinant = jw_slater_determinant(slater_determinant_matrix)
+        self.assertAlmostEqual(slater_determinant[1], slater_determinant[2])
+        self.assertAlmostEqual(abs(slater_determinant[1]), 1. / numpy.sqrt(2.))
+        self.assertAlmostEqual(abs(slater_determinant[0]), 0.)
+        self.assertAlmostEqual(abs(slater_determinant[3]), 0.)
+
+        slater_determinant_matrix = numpy.array([[1., -1.]]) / numpy.sqrt(2.)
+        slater_determinant = jw_slater_determinant(slater_determinant_matrix)
+        self.assertAlmostEqual(slater_determinant[1], -slater_determinant[2])
+        self.assertAlmostEqual(abs(slater_determinant[1]), 1. / numpy.sqrt(2.))
+        self.assertAlmostEqual(abs(slater_determinant[0]), 0.)
+        self.assertAlmostEqual(abs(slater_determinant[3]), 0.)
+
+
+class GaussianStatePreparationCircuitTest(unittest.TestCase):
+
+    def setUp(self):
+        self.n_qubits_range = range(3, 6)
+
+    def test_ground_state_particle_conserving(self):
+        """Test getting the ground state preparation circuit for a Hamiltonian
+        that conserves particle number."""
+        for n_qubits in self.n_qubits_range:
+            # Initialize a particle-number-conserving Hamiltonian
+            quadratic_hamiltonian = random_quadratic_hamiltonian(
+                n_qubits, True, True)
+
+            # Compute the true ground state
+            sparse_operator = get_sparse_operator(quadratic_hamiltonian)
+            ground_energy, _ = get_ground_state(sparse_operator)
+
+            # Obtain the circuit
+            circuit_description, start_orbitals = (
+                gaussian_state_preparation_circuit(quadratic_hamiltonian))
+
+            # Initialize the starting state
+            state = jw_configuration_state(start_orbitals, n_qubits)
+
+            # Apply the circuit
+            for parallel_ops in circuit_description:
+                for op in parallel_ops:
+                    self.assertTrue(op != 'pht')
+                    i, j, theta, phi = op
+                    state = jw_sparse_givens_rotation(i, j, theta, phi,
+                                                      n_qubits).dot(state)
+
+            # Check that the state obtained using the circuit is a ground state
+            difference = sparse_operator * state - ground_energy * state
+            discrepancy = numpy.amax(numpy.abs(difference))
+            self.assertAlmostEqual(discrepancy, 0)
+
+    def test_ground_state_particle_nonconserving(self):
+        """Test getting the ground state preparation circuit for a Hamiltonian
+        that does not conserve particle number."""
+        for n_qubits in self.n_qubits_range:
+            # Initialize a particle-number-conserving Hamiltonian
+            quadratic_hamiltonian = random_quadratic_hamiltonian(
+                n_qubits, False, True)
+
+            # Compute the true ground state
+            sparse_operator = get_sparse_operator(quadratic_hamiltonian)
+            ground_energy, _ = get_ground_state(sparse_operator)
+
+            # Obtain the circuit
+            circuit_description, start_orbitals = (
+                gaussian_state_preparation_circuit(quadratic_hamiltonian))
+
+            # Initialize the starting state
+            state = jw_configuration_state(start_orbitals, n_qubits)
+
+            # Apply the circuit
+            particle_hole_transformation = (
+                jw_sparse_particle_hole_transformation_last_mode(n_qubits))
+            for parallel_ops in circuit_description:
+                for op in parallel_ops:
+                    if op == 'pht':
+                        state = particle_hole_transformation.dot(state)
+                    else:
+                        i, j, theta, phi = op
+                        state = jw_sparse_givens_rotation(
+                            i, j, theta, phi, n_qubits).dot(state)
+
+            # Check that the state obtained using the circuit is a ground state
+            difference = sparse_operator * state - ground_energy * state
+            discrepancy = numpy.amax(numpy.abs(difference))
+            self.assertAlmostEqual(discrepancy, 0)
+
+    def test_bad_input(self):
+        """Test bad input."""
+        with self.assertRaises(ValueError):
+            gaussian_state_preparation_circuit('a')
+
+
+class JWGetGaussianStateTest(unittest.TestCase):
+
+    def setUp(self):
+        self.n_qubits_range = range(2, 10)
+
+    def test_ground_state_particle_conserving(self):
+        """Test getting the ground state of a Hamiltonian that conserves
+        particle number."""
+        for n_qubits in self.n_qubits_range:
+            # Initialize a particle-number-conserving Hamiltonian
+            quadratic_hamiltonian = random_quadratic_hamiltonian(n_qubits, True)
+
+            # Compute the true ground state
+            sparse_operator = get_sparse_operator(quadratic_hamiltonian)
+            ground_energy, _ = get_ground_state(sparse_operator)
+
+            # Compute the ground state using the circuit
+            circuit_energy, circuit_state = jw_get_gaussian_state(
+                quadratic_hamiltonian)
+
+            # Check that the energies match
+            self.assertAlmostEqual(ground_energy, circuit_energy)
+
+            # Check that the state obtained using the circuit is a ground state
+            difference = (sparse_operator * circuit_state -
+                          ground_energy * circuit_state)
+            discrepancy = numpy.amax(numpy.abs(difference))
+            self.assertAlmostEqual(discrepancy, 0)
+
+    def test_ground_state_particle_nonconserving(self):
+        """Test getting the ground state of a Hamiltonian that does not
+        conserve particle number."""
+        for n_qubits in self.n_qubits_range:
+            # Initialize a non-particle-number-conserving Hamiltonian
+            quadratic_hamiltonian = random_quadratic_hamiltonian(
+                n_qubits, False)
+
+            # Compute the true ground state
+            sparse_operator = get_sparse_operator(quadratic_hamiltonian)
+            ground_energy, _ = get_ground_state(sparse_operator)
+
+            # Compute the ground state using the circuit
+            circuit_energy, circuit_state = (
+                jw_get_gaussian_state(quadratic_hamiltonian))
+
+            # Check that the energies match
+            self.assertAlmostEqual(ground_energy, circuit_energy)
+
+            # Check that the state obtained using the circuit is a ground state
+            difference = (sparse_operator * circuit_state -
+                          ground_energy * circuit_state)
+            discrepancy = numpy.amax(numpy.abs(difference))
+            self.assertAlmostEqual(discrepancy, 0)
+
+    def test_excited_state_particle_conserving(self):
+        """Test getting an excited state of a Hamiltonian that conserves
+        particle number."""
+        for n_qubits in self.n_qubits_range:
+            # Initialize a particle-number-conserving Hamiltonian
+            quadratic_hamiltonian = random_quadratic_hamiltonian(n_qubits, True)
+
+            # Pick some orbitals to occupy
+            num_occupied_orbitals = numpy.random.randint(1, n_qubits + 1)
+            occupied_orbitals = numpy.random.choice(range(n_qubits),
+                                                    num_occupied_orbitals,
+                                                    False)
+
+            # Compute the Gaussian state
+            circuit_energy, gaussian_state = jw_get_gaussian_state(
+                quadratic_hamiltonian, occupied_orbitals)
+
+            # Compute the true energy
+            orbital_energies, constant = (
+                quadratic_hamiltonian.orbital_energies())
+            energy = numpy.sum(orbital_energies[occupied_orbitals]) + constant
+
+            # Check that the energies match
+            self.assertAlmostEqual(energy, circuit_energy)
+
+            # Check that the state obtained using the circuit is an eigenstate
+            # with the correct eigenvalue
+            sparse_operator = get_sparse_operator(quadratic_hamiltonian)
+            difference = (sparse_operator * gaussian_state -
+                          energy * gaussian_state)
+            discrepancy = numpy.amax(numpy.abs(difference))
+            self.assertAlmostEqual(discrepancy, 0)
+
+    def test_excited_state_particle_nonconserving(self):
+        """Test getting an excited state of a Hamiltonian that conserves
+        particle number."""
+        for n_qubits in self.n_qubits_range:
+            # Initialize a non-particle-number-conserving Hamiltonian
+            quadratic_hamiltonian = random_quadratic_hamiltonian(
+                n_qubits, False)
+
+            # Pick some orbitals to occupy
+            num_occupied_orbitals = numpy.random.randint(1, n_qubits + 1)
+            occupied_orbitals = numpy.random.choice(range(n_qubits),
+                                                    num_occupied_orbitals,
+                                                    False)
+
+            # Compute the Gaussian state
+            circuit_energy, gaussian_state = jw_get_gaussian_state(
+                quadratic_hamiltonian, occupied_orbitals)
+
+            # Compute the true energy
+            orbital_energies, constant = (
+                quadratic_hamiltonian.orbital_energies())
+            energy = numpy.sum(orbital_energies[occupied_orbitals]) + constant
+
+            # Check that the energies match
+            self.assertAlmostEqual(energy, circuit_energy)
+
+            # Check that the state obtained using the circuit is an eigenstate
+            # with the correct eigenvalue
+            sparse_operator = get_sparse_operator(quadratic_hamiltonian)
+            difference = (sparse_operator * gaussian_state -
+                          energy * gaussian_state)
+            discrepancy = numpy.amax(numpy.abs(difference))
+            self.assertAlmostEqual(discrepancy, 0)
+
+    def test_bad_input(self):
+        """Test bad input."""
+        with self.assertRaises(ValueError):
+            jw_get_gaussian_state('a')
+
+
+def test_not_implemented_spinr_reduced():
+    """Tests that currently un-implemented functionality is caught."""
+    msg = "Specifying spin sector for non-particle-conserving "
+    msg += "Hamiltonians is not yet supported."
+    for n_qubits in [2, 4, 6]:
+        # Initialize a particle-number-conserving Hamiltonian
+        quadratic_hamiltonian = random_quadratic_hamiltonian(
+            n_qubits, False, True)
+
+        # Obtain the circuit
+        with pytest.raises(NotImplementedError):
+            _ = gaussian_state_preparation_circuit(quadratic_hamiltonian,
+                                                   spin_sector=1)
