6563df9539b869c5b915d244705b8806b211b7a0
Tom O'Brien <teobrien@google.com>
2020-10-29 16:03:33 +0100

    Added reference to vpe paper

--- src/openfermion/measurements/vpe_estimators.py
+++ src/openfermion/measurements/vpe_estimators.py
@@ -9,7 +9,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-"""Classes to assist in processing of VPE signal data"""
+"""Classes to assist in processing of VPE (ArXiv:2010.02538) signal data"""
 
 import abc
 import numpy
