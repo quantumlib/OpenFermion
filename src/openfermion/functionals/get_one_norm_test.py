c203f67334afe5f7c26f30441b1487b7b60afb4c
Emieeel <efkoridon@gmail.com>
2021-05-06 12:47:21 +0200

    changed from unittest to pytest

--- src/openfermion/functionals/get_one_norm_test.py
+++ src/openfermion/functionals/get_one_norm_test.py
@@ -12,14 +12,14 @@
 """Tests for get_one_norm."""
 
 import os
-import unittest
+import pytest
 from openfermion import get_one_norm, MolecularData, jordan_wigner
 from openfermion.config import DATA_DIRECTORY
 
 
-class test_get_one_norm(unittest.TestCase):
+class Testclass():
 
-    def setUp(self):
+    def __init__(self):
         self.filename = os.path.join(DATA_DIRECTORY,
                                      "H1-Li1_sto-3g_singlet_1.45.hdf5")
         self.molecule = MolecularData(filename=self.filename)
@@ -27,15 +27,15 @@ class test_get_one_norm(unittest.TestCase):
         self.qubit_hamiltonian = jordan_wigner(self.molecular_hamiltonian)
 
     def test_one_norm_from_molecule(self):
-        self.assertAlmostEqual(self.qubit_hamiltonian.induced_norm(),
-                               get_one_norm(self.molecule))
+        assert self.qubit_hamiltonian.induced_norm() == pytest.approx(
+            get_one_norm(self.molecule))
 
     def test_one_norm_from_ints(self):
         ints = (self.molecule.nuclear_repulsion,
                 self.molecule.one_body_integrals,
                 self.molecule.two_body_integrals)
-        self.assertAlmostEqual(self.qubit_hamiltonian.induced_norm(),
-                               get_one_norm(ints))
+        assert self.qubit_hamiltonian.induced_norm() == pytest.approx(
+            get_one_norm(ints))
 
     def test_one_norm_woconst(self):
         one_norm_woconst = (self.qubit_hamiltonian.induced_norm() -
@@ -43,8 +43,7 @@ class test_get_one_norm(unittest.TestCase):
         ints = (self.molecule.nuclear_repulsion,
                 self.molecule.one_body_integrals,
                 self.molecule.two_body_integrals)
-        self.assertAlmostEqual(
-            one_norm_woconst, get_one_norm(self.molecule,
-                                           return_constant=False))
-        self.assertAlmostEqual(one_norm_woconst,
-                               get_one_norm(ints, return_constant=False))
+        assert one_norm_woconst == pytest.approx(
+            get_one_norm(self.molecule, return_constant=False))
+        assert one_norm_woconst == pytest.approx(
+            get_one_norm(ints, return_constant=False))
