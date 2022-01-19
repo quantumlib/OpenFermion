6acf9439f4a965080fc31e7c0ad20b1f00820991
jjgoings <jjgoings@gmail.com>
2022-01-19 10:32:35 -0800

    clean up imports

--- src/openfermion/resource_estimates/thc/utils/__init__.py
+++ src/openfermion/resource_estimates/thc/utils/__init__.py
@@ -13,21 +13,11 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from .thc_factorization import (lbfgsb_opt_thc,
-                                lbfgsb_opt_thc_l2reg,
-                                adagrad_opt_thc,
-                                lbfgsb_opt_cholesky)
-from .thc_objectives import (thc_objective_jax,
-                             thc_objective_grad_jax,
-                             thc_objective,
-                             thc_objective_regularized,
-                             thc_objective_grad,
-                             thc_objective_and_grad,
+from .thc_factorization import (lbfgsb_opt_thc, lbfgsb_opt_thc_l2reg,
+                                adagrad_opt_thc, lbfgsb_opt_cholesky)
+from .thc_objectives import (thc_objective_jax, thc_objective_grad_jax,
+                             thc_objective, thc_objective_regularized,
+                             thc_objective_grad, thc_objective_and_grad,
                              cp_ls_cholesky_factor_objective)
-from .adagrad import (adagrad,
-                      constant,
-                      exponential_decay,
-                      inverse_time_decay,
-                      polynomial_decay,
-                      piecewise_constant,
-                      make_schedule)
+from .adagrad import (adagrad, constant, exponential_decay, inverse_time_decay,
+                      polynomial_decay, piecewise_constant, make_schedule)
