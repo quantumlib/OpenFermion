865bc3a639df1cbdbf821d522106145785e131c1
jjgoings <jjgoings@gmail.com>
2022-01-20 10:13:50 -0800

    fixup some old comments

--- src/openfermion/resource_estimates/thc/utils/adagrad.py
+++ src/openfermion/resource_estimates/thc/utils/adagrad.py
@@ -12,61 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-JAX free implementation of Adagrad
-
-Timing indicates lots of XLA stuff happening that I don't want in the
-jax implementation.
-
-Pure python implementaiton here.
-
-Optimizers for use with JAX (but removing JAX).
-This module contains some convenient optimizer definitions, specifically
-initialization and update functions, which can be used with ndarrays or
-arbitrarily-nested tuple/list/dicts of ndarrays.
-An optimizer is modeled as an ``(init_fun, update_fun, get_params)`` triple of
-functions, where the component functions have these signatures:
-::
-  init_fun(params)
-  Args:
-    params: pytree representing the initial parameters.
-  Returns:
-    A pytree representing the initial optimizer state, which includes the
-    initial parameters and may also include auxiliary values like initial
-    momentum. The optimizer state pytree structure generally differs from that
-    of `params`.
-::
-  update_fun(step, grads, opt_state)
-  Args:
-    step: integer representing the step index.
-    grads: a pytree with the same structure as `get_params(opt_state)`
-      representing the gradients to be used in updating the optimizer state.
-    opt_state: a pytree representing the optimizer state to be updated.
-  Returns:
-    A pytree with the same structure as the `opt_state` argument representing
-    the updated optimizer state.
-::
-  get_params(opt_state)
-  Args:
-    opt_state: pytree representing an optimizer state.
-  Returns:
-    A pytree representing the parameters extracted from `opt_state`, such that
-    the invariant `params == get_params(init_fun(params))` holds true.
-Notice that an optimizer implementation has a lot of flexibility in the form of
-opt_state: it just has to be a pytree of JaxTypes (so that it can be passed to
-the JAX transforms defined in api.py) and it has to be consumable by update_fun
-and get_params.
-Example Usage:
-.. code-block:: python
-  opt_init, opt_update, get_params = optimizers.sgd(learning_rate)
-  opt_state = opt_init(params)
-  def step(step, opt_state):
-    value, grads = jax.value_and_grad(loss_fn)(get_params(opt_state))
-    opt_state = opt_update(step, grads, opt_state)
-    return value, opt_state
-  for i in range(num_steps):
-    value, opt_state = step(i, opt_state)
-"""
 from typing import Any, Callable, Union
 import numpy as np
 
