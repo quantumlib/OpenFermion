13a505bbf757231c7c72588191ab8955205883f3
Fionn Malone <fmalone@google.com>
2023-06-20 20:20:25 +0000

    Add missing init / skipifs.

--- src/openfermion/resource_estimates/pbc/thc/utils/__init__.py
+++ src/openfermion/resource_estimates/pbc/thc/utils/__init__.py
@@ -10,3 +10,12 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+import pytest
+
+try:
+    import jax
+    import pyscf
+except (ImportError, ModuleNotFoundError) as err:
+    pytest.skip(
+        f"Need pyscf and jax for PBC resource estimates {err}", allow_module_level=True
+    )
