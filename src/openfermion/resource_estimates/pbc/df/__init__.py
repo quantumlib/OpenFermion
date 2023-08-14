69e6e54b525bc78c9b0871fc4143cba959c6df94
Fionn Malone <fmalone@google.com>
2023-08-14 22:31:01 +0000

    Fix inits.

--- src/openfermion/resource_estimates/pbc/df/__init__.py
+++ src/openfermion/resource_estimates/pbc/df/__init__.py
@@ -10,10 +10,3 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-import pytest
-
-try:
-    import pyscf
-except (ImportError, ModuleNotFoundError) as err:
-    pytest.skip(f"Need pyscf for PBC resource estimates {err}",
-                allow_module_level=True)
