4504bb34cbb1125cdd166b1aed434b703fdd88e8
Fionn Malone <fmalone@google.com>
2023-08-21 16:42:30 +0000

    Add missing import.

--- src/openfermion/resource_estimates/pbc/df/generate_costing_table_df_test.py
+++ src/openfermion/resource_estimates/pbc/df/generate_costing_table_df_test.py
@@ -11,6 +11,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 import numpy as np
+import pytest
 
 from openfermion.resource_estimates import HAVE_DEPS_FOR_RESOURCE_ESTIMATES
 
