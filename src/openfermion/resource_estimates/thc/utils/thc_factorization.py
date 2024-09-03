b6191ab7eca8f83ff11c4207ddcbff211e83c263
Maximilian Scheurer <104848085+cvsik@users.noreply.github.com>
2024-09-03 18:42:27 +0200

    JAX config updates and expose `penalty_param` (#894)
    * jax config updates and expose penalty_param
    
    bump jax version
    
    * make linter and formatter happy

--- src/openfermion/resource_estimates/thc/utils/thc_factorization.py
+++ src/openfermion/resource_estimates/thc/utils/thc_factorization.py
@@ -1,4 +1,5 @@
 # coverage:ignore
+# pylint: disable=wrong-import-position
 import os
 from uuid import uuid4
 import h5py
@@ -6,9 +7,12 @@ import numpy
 import numpy.random
 import numpy.linalg
 from scipy.optimize import minimize
+
 import jax
+
+jax.config.update("jax_enable_x64", True)
+
 import jax.numpy as jnp
-from jax.config import config
 from jax import jit, grad
 from .adagrad import adagrad
 from .thc_objectives import (
@@ -22,7 +26,6 @@ from .thc_objectives import (
 # set mkl thread count for numpy einsum/tensordot calls
 # leave one CPU un used  so we can still access this computer
 os.environ["MKL_NUM_THREADS"] = "{}".format(os.cpu_count() - 1)
-config.update("jax_enable_x64", True)
 
 
 class CallBackStore:
--- src/openfermion/resource_estimates/thc/utils/thc_objectives.py
+++ src/openfermion/resource_estimates/thc/utils/thc_objectives.py
@@ -1,9 +1,14 @@
 # coverage:ignore
+# pylint: disable=wrong-import-position
 import os
 from uuid import uuid4
 import scipy.optimize
+
+import jax
+
+jax.config.update("jax_enable_x64", True)
+
 import jax.numpy as jnp
-from jax.config import config
 from jax import jit, grad
 import h5py
 import numpy
@@ -15,7 +20,6 @@ from .adagrad import adagrad
 # set mkl thread count for numpy einsum/tensordot calls
 # leave one CPU un used  so we can still access this computer
 os.environ["MKL_NUM_THREADS"] = "{}".format(os.cpu_count() - 1)
-config.update("jax_enable_x64", True)
 
 
 def thc_objective_jax(xcur, norb, nthc, eri):
