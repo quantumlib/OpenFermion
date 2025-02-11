18d548278b13333ce11d699656b5004e5fe66a7d
Michael Hucka <mhucka@google.com>
2025-02-11 12:13:01 -0800

    Fix code format

--- setup.py
+++ setup.py
@@ -62,8 +62,7 @@ setup(
     include_package_data=True,
     package_data={
         '': [
-            os.path.join('src', 'openfermion', 'resource_estimates',
-                         'integrals', '*.h5'),
+            os.path.join('src', 'openfermion', 'resource_estimates', 'integrals', '*.h5'),
             os.path.join('src', 'openfermion', 'testing', '*.npy'),
             os.path.join('src', 'openfermion', 'testing', '*.hdf5'),
         ]
