298399c3cdc5d47eddc96f6ad12045f2601e738c
Aniket Singh Yadav <singhyadavaniket43@gmail.com>
2025-10-13 12:28:17 +0530

    Fix get_sparse_operator to handle unsimplified QubitOperator

--- src/openfermion/linalg/sparse_tools.py
+++ src/openfermion/linalg/sparse_tools.py
@@ -18,6 +18,7 @@ import numpy
 import scipy
 import scipy.sparse
 import scipy.sparse.linalg
+import warnings
 
 from openfermion.ops.operators import FermionOperator, QubitOperator, BosonOperator, QuadOperator
 from openfermion.ops.representations import DiagonalCoulombHamiltonian, PolynomialTensor
@@ -1268,7 +1269,13 @@ def get_sparse_operator(operator, n_qubits=None, trunc=None, hbar=1.0):
     elif isinstance(operator, FermionOperator):
         return jordan_wigner_sparse(operator, n_qubits)
     elif isinstance(operator, QubitOperator):
-        operator.simplify()
+        if hasattr(operator, "simplify"):
+            operator.simplify()
+        else:
+            warnings.warn(
+            "QubitOperator.simplify() not found. "
+            "Ensure your operator is simplified to avoid errors."
+        )
         return qubit_operator_sparse(operator, n_qubits)
     elif isinstance(operator, (BosonOperator, QuadOperator)):
         return boson_operator_sparse(operator, trunc, hbar)
