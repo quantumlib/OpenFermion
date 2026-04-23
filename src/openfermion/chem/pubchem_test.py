dd9f0dd840fe50ae4cdb30a803205aed3119b0ab
mhucka <mhucka@google.com>
2026-04-23 02:57:30 +0000

    Handle retries on exceptions more properly

--- src/openfermion/chem/pubchem_test.py
+++ src/openfermion/chem/pubchem_test.py
@@ -152,10 +152,7 @@ class OpenFermionPubChemTest(unittest.TestCase):
         with pytest.raises(ValueError, match='Incorrect value for the argument structure'):
             _ = geometry_from_pubchem('water', structure='foo')
 
-    @pytest.mark.flaky(retries=3, delay=2)
+    @pytest.mark.flaky(retries=3, delay=2, only_on=[pubchempy.ServerBusyError])
     def test_geometry_from_pubchem_live_api(self):
-        try:
-            water_geometry = geometry_from_pubchem('water')  # pragma: no cover
-            self.assertEqual(len(water_geometry), 3)  # pragma: no cover
-        except pubchempy.ServerBusyError:  # pragma: no cover
-            pytest.skip('PubChem server is busy.')
+        water_geometry = geometry_from_pubchem('water')
+        self.assertEqual(len(water_geometry), 3)
