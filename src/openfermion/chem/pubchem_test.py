f2108303ebb86cef32acf6ad2e0c9f8d8cb7b15d
mhucka <mhucka@google.com>
2026-04-23 17:59:25 +0000

    Increase flaky test retry detail
    CI is still failing with server busy errors. Maybe the problem is the
    retries are happening too close together?

--- src/openfermion/chem/pubchem_test.py
+++ src/openfermion/chem/pubchem_test.py
@@ -152,7 +152,7 @@ class OpenFermionPubChemTest(unittest.TestCase):
         with pytest.raises(ValueError, match='Incorrect value for the argument structure'):
             _ = geometry_from_pubchem('water', structure='foo')
 
-    @pytest.mark.flaky(retries=3, delay=2, only_on=[pubchempy.ServerBusyError])
+    @pytest.mark.flaky(retries=3, delay=10, only_on=[pubchempy.ServerBusyError])
     def test_geometry_from_pubchem_live_api(self):
         water_geometry = geometry_from_pubchem('water')
         self.assertEqual(len(water_geometry), 3)
