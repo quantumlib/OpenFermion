5708ab4330d10ccfb08321cff73af519684bb01e
mhucka <mhucka@google.com>
2026-04-28 00:48:52 +0000

    Mark another slow test

--- src/openfermion/resource_estimates/pbc/thc/factorizations/isdf_test.py
+++ src/openfermion/resource_estimates/pbc/thc/factorizations/isdf_test.py
@@ -10,6 +10,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+
 import numpy as np
 import pytest
 
@@ -252,6 +253,7 @@ def test_G_vector_mapping_single_translation():
                 assert np.allclose(delta_G_expected, delta_G)
 
 
+@pytest.mark.slow
 @pytest.mark.skipif(not HAVE_DEPS_FOR_RESOURCE_ESTIMATES, reason='pyscf and/or jax not installed.')
 def test_kpoint_isdf_double_translation():
     cell = gto.Cell()
