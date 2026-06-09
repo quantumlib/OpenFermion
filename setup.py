15388e5ce63068504445c05b3e9338e3d34992bc
Christ Beaubrun <beaubrunbusiness@gmail.com>
2026-06-08 22:35:35 -0500

    Restore license classifier with SPDX metadata

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: MacOS',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
