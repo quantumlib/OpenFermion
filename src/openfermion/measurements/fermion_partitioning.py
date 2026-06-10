02f87de12e0f6379b82f62dda18a4e3f8a117b7c
ツンデレ <vpeilivanidis@gmail.com>
2026-06-10 15:11:49 +0300

    Update src/openfermion/measurements/fermion_partitioning.py
    Co-authored-by: Michael Hucka <mhucka@google.com>

--- src/openfermion/measurements/fermion_partitioning.py
+++ src/openfermion/measurements/fermion_partitioning.py
@@ -244,7 +244,7 @@ def pair_within_simultaneously(labels: list[T]) -> Generator[Pairing[T], None, N
     for partition in _gen_partitions(labels):
         generator_list = [_loop_iterator(pair_within, partition[j]) for j in range(len(partition))]
         for dummy1 in range(len(partition[-2]) - 1 + len(partition[-2]) % 2):
-            pairing: Pairing = ()
+            pairing: Pairing[T] = ()
             for generator in generator_list[::2]:
                 pairing = pairing + next(generator)[0]
             for dummy2 in range(len(partition[-1]) - 1 + len(partition[-1]) % 2):
